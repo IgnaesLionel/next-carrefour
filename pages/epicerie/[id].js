29ee973665cf63a4c035ea191b8ecd7e6f39f2e4
ignaesLionel <ignaes_lionel@hotmail.com>
2021-06-29 19:09:31 +0200

    url fix

--- pages/epicerie/[id].js
+++ pages/epicerie/[id].js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import axios from "axios";
 import styles from "../../styles/Home.module.css";
+import Link from "next/link";
 
 export default function DetailPage({ res }) {
   const router = useRouter();
@@ -84,15 +85,17 @@ export default function DetailPage({ res }) {
   return (
     <div className={styles.containerId}>
       <h1>Horraire de la semaine {res.semaine} </h1>
-      <Image
-        quality={40}
-        width={1000}
-        height={800}
-        src={`${res.url}`}
-        alt="horraire"
-        placeholder="blur"
-        blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
-      ></Image>
+      <Link href={`${res.url}`}>
+        <Image
+          quality={40}
+          width={1000}
+          height={800}
+          src={`${res.url}`}
+          alt="horraire"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
+        ></Image>
+      </Link>
       <form>
         <label> image</label>
         <input type="file" accept=".jpg, .jpeg, .png" onChange={selectFile} />
